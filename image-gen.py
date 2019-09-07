0dc4343f5fc7d37e74dd3645b384c559b2246662
lord-vorian <highskieseverywhere@gmail.com>
2019-09-07 15:55:56 -0600

    set up column ranges for splitting image1 into columns

--- image-gen.py
+++ image-gen.py
@@ -14,7 +14,7 @@ window_length = 256  # days
 goal = 2  # daily goal for contributions. Big effect on image generated
 rel_contributions = []  # rel = relevant
 
-call([r'scrape.bat'])  # this bat uses the submodded scraper to output "contributions.json'
+#call([r'scrape.bat'])  # this bat uses the submodded scraper to output "contributions.json'
 
 
 with open('contributions.json') as source:
@@ -26,8 +26,29 @@ with open('contributions.json') as source:
     rel_contributions = all_contributions[-1:0-window_length-1:-1]  # step backward from most recent
     print('.json file parsed...')
 
-image1 = Image.open('img\image1.jpg')
+image1 = Image.open('img\image1.jpg')  # TODO make this filetype agnostic
 width, height = image1.size
-print(width/window_length)
+
+column_ranges = []  # a set of pixel locations in x to be used as bars in the chart
+column_width = width//window_length  # number of pixels in each column
+
+for column in range(0, window_length):  # give each column a single value: its width
+    if column <= width % window_length:
+        column_ranges.append([column_width+1])  # distribute remainder of pixels across remainder(th) columns
+    else:                                       # ex: 1920 % 256 = 128. So add +1 to the first 128 columns
+        column_ranges.append([column_width])
+
+pixel_position = -1  # store pixel position across image. -1 so we arrive at 0 on first iteration
+for column in range(0,len(column_ranges)):
+    # turn this list of pixel widths into a list of pixel ranges
+    column_ranges[column].append(column_ranges[column][0] + pixel_position)
+    # add the width of this column to the position on the image to get the end of the range
+    column_ranges[column][0] = pixel_position + 1
+    # we no longer need the width, so we replace it with the current position + 1 pixel
+    pixel_position = column_ranges[column][1]
+    # update current position so it matched
+print(column_ranges)
+
+
 
 
